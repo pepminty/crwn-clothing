71ee3a913aeafda91b99cb0abefa2b5213c7c00c
Pepminty <pip.minty@gmail.com>
2021-03-08 14:59:24 +0800

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component'
+import { Route, Switch } from 'react-router-dom';
+import HatsPage from './components/pages/hats/hats.component';
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
