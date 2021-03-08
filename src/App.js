01500134404f9143f03fa3b16ffddae9d07c5e44
Pepminty <pip.minty@gmail.com>
2021-03-08 15:57:57 +0800

    Shop page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom';
-import HatsPage from './components/pages/hats/hats.component';
+import ShopePage from './components/pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopePage} />
       </Switch>
     </div>
   );
