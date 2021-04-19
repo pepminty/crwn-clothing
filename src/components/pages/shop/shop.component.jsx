db107391029c7534059deb3b46a15aed44026a42
Pipminty <pip.minty@gmail.com>
2021-04-19 17:11:45 +0800

    firebase update etc

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -2,14 +2,41 @@ import React from "react";
 import CollectionsOverview from "../../collections-overview/collections-overview.component";
 import { Route } from "react-router-dom";
 import Collection from "../collection/collection.component";
+import { connect } from "react-redux";
+import { updateCollections } from "./../../../redux/shop/shop.actions";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../../firebase/firebase.utils";
 
-const ShopePage = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
-};
+class ShopePage extends React.Component {
+  unsubscribeFromSnapshot = null;
 
-export default ShopePage;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+      async (snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+      }
+    );
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopePage);
